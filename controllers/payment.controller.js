2ffa47e5faf6f0b0df194ea9213b69e342c3938a
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-11-20 13:26:07 +0530

    controller - getRazorpayApiKey done

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,4 +1,11 @@
-export const getRazorpayApiKey = async (req, res, next) => {};
+export const getRazorpayApiKey = async (req, res, next) => {
+  // first we need to return razorpay key to client
+  req.status(200).josn({
+    success: true,
+    message: "Razorpay API Key",
+    key: process.env.RAZORPAY_KEY_ID,
+  });
+};
 
 export const buySubscrition = async (req, res, next) => {};
 
